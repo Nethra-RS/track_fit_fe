396622071fc6e257748e2ae90ee2aded0774218d
rrm003 <rammankar003@gmail.com>
2025-03-10 20:25:08 -0500

    responsive reset password confirmation

--- src/RequestConfirmation.js
+++ src/RequestConfirmation.js
@@ -1,28 +1,36 @@
 import React from "react";
 import "./RequestConfirmation.css";
-import image from "./image 4.png"; // Use the actual image path
+import image from "./image 4.png";
 
 const ResetPasswordConfirmation = () => {
   return (
     <div className="resend-container">
-      <div className = "background-new"></div>
-      <h2 className="logo1"><span className="trackfit1">trackfit</span><span className="dot1">.</span></h2>
+      <div className="background-new"></div>
+      <h2 className="logo1">
+        <span className="trackfit1">trackfit</span>
+        <span className="dot1">.</span>
+      </h2>
+
       <div className="resend-box">
-        {/* Left Side - Text */}
+        {/* Image Section - Visible on larger screens only */}
+        <div className="resend-image">
+          <img src={image} alt="Resend Password" />
+        </div>
+
+        {/* Form Section */}
         <div className="resend-form">
-          <h2 className="reset">Reset Password.</h2>
-          <p>Instructions to reset your password have been sent to your email.</p>
-          <button className="resend">          
+          <h2 className="reset">Reset Password</h2>
+          <p>
+            Instructions to reset your password have been sent to your email.
+          </p>
+
+          <button className="resend">
             <a href="/ForgotPassword">Resend Email</a>
           </button>
+
           <button className="goback-btn">
             <a href="/signin">Go Back to Login</a>
           </button>
-        </div>
-
-        {/* Right Side - Image */}
-        <div className="resend-image">
-          <img src={image} alt="Resend Password" />
         </div>
       </div>
     </div>
